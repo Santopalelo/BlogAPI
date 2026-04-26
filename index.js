2400846390f6a4126cc34674f68942c2715eb2fa
Palelo <onwesomtolawrence@gmail.com>
2026-04-26 22:47:05 +0100

    refactored

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require("./database/database.js")
-const logRequest = require('./middlewares/logger.js');
-const errorHandler = require('./middlewares/errorHandler.js');
-const articleRoutes = require('./routes/article.route.js');
-const userRoutes = require('./routes/user.route.js');
+require("dotenv").config()
 
-const app = express();
-app.use(cors("*"));
-app.use(express.json());
-const PORT = process.env.PORT || 3000;
-
+const app = require("./SRC/app");
+const connectDB = require("./SRC/config/connectDB.js");
 connectDB();
-app.use(logRequest);
-app.use("/api", articleRoutes)
-app.use("/api/users", userRoutes)
 
-app.use(errorHandler)
-app.listen(PORT, ()=>{
+const PORT=process.env.PORT||3000;
+app.listen(PORT, async()=>{
     console.log(`Server is running on port ${PORT}`);
 })
--- middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) =>{
-console.error(err.message)
-const status = err.status || 500
-res.status(status).json({error: err.message})
-}
-module.exports = errorHandler
